65823a87b577b9b1096db6118737108e49b65903
Thaironius <thaironius@gmail.com>
2013-06-02 18:07:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlumniKickstarter::Application.config.secret_token = 'b321bb713b34e12ae16aab3e3a9f064434c67b2ac27f145d08400be0129b12e85382a0ca2ac085b3066f829eb91789322f9d9df6f8e9ec82aabbc20e8b735584'
